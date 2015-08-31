c70b6ad9374a211196c8dce598230b89a74dcf41
nehachaube <nehachaube30@gmail.com>
2015-08-30 20:48:20 -0500

    adding logic to Connect client to server,sending queries and read response from server

--- src/ClientLogQuerier.java
+++ src/ClientLogQuerier.java
@@ -1,11 +1,15 @@
 import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.*;
 import java.nio.channels.*;
 import java.util.*;
 
 
 public class ClientLogQuerier {
 	
-	ArrayList<Catalog> machine_info;
+	static ArrayList<Catalog> machine_info;
+	Socket socketClient=null;
+	//reading host name and port number and storing in an arraylist of class Catalog
 	public ClientLogQuerier(String filePath) 
 	{
 		try{
@@ -19,12 +23,39 @@ public class ClientLogQuerier {
         	c= new Catalog(ar[0],ar[1]);
         	machine_info.add(c);        	
         }
-        c.toString();
+        System.out.println(machine_info);
 		}
 		catch(IOException e)
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	//connecting to server
+	public void connect(String hostName,String portName) throws UnknownHostException, IOException
+	{
+		System.out.println("Connecting to "+hostName+" "+portName);
+		socketClient=new Socket(hostName,Integer.parseInt(portName));
+		System.out.println("Connection Established to"+hostName);
+		
+	}
+	//reading response from server
+	public void readResponsefromServer() throws IOException{
+	       String userInput;
+	       BufferedReader stdIn = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+
+	       System.out.print("RESPONSE FROM SERVER:");
+	       while ((userInput = stdIn.readLine()) != null) {
+	           System.out.println(userInput);
+	       }
+	    }
+	//sending query to server
+	public void grepQuery() throws IOException
+	{
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
+		 writer.write("grep?");
+	       writer.newLine();
+	       writer.flush();
 	}
 
     public static void main(String[] args) throws IOException {
@@ -37,9 +68,32 @@ public class ClientLogQuerier {
     	//registering channels with the selector
     	channel.configureBlocking(false);
 
-    	SelectionKey key = channel.register(selector, SelectionKey.OP_READ);*/
-    	System.out.println("Enter");
+    	SelectionKey key = channel.register(selector, SelectionKey.OP_READ);
+    	 SocketChannel channel = SocketChannel.open();
+    	 // we open this channel in non blocking mode
+         channel.configureBlocking(false);
+         channel.connect(new InetSocketAddress("localhost", port));*/
+    	//Creating a ClientLogQuerier object
     	ClientLogQuerier c= new ClientLogQuerier("src\\machineinfo");
+    	try{
+    	for(Catalog c1:machine_info)
+    	{
+    		//trying to establish connection to server
+    		c.connect(c1.getHostName(), c1.getPortName());
+    		//sending query to server
+    		c.grepQuery();
+    		//waiting to read response from server
+            c.readResponsefromServer();
+    		
+    	}
+    	}
+    	catch (UnknownHostException e) {
+            System.err.println("Host unknown. Cannot establish connection");
+        } catch (IOException e) {
+            System.err.println("Cannot establish connection. Server may not be up."+e.getMessage());
+        }
+        
+    	
     }
 
 }
