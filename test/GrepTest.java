7bb17c4615bca106374f86e986e5388bfea4f0b5
nehachaube <nehachaube30@gmail.com>
2015-09-08 00:45:55 -0500

    initial design of unit test case

--- test/GrepTest.java
+++ test/GrepTest.java
@@ -7,6 +7,35 @@ public class GrepTest {
     @Test
     public void test() {
         fail("Not yet implemented");
+        
     }
 
+    /**Method to generate log files dynamically.
+     * The log file will be generated such that they have very rare,rare,somewhat frequent
+     * and frequent patterns
+     */
+     public void generateLogFile(){
+         
+     }
+     /** Method to call the distributed log querier function to test the grep functionality
+      * 
+      */
+     public void distributedLogQuerier()
+     {
+         
+     }
+     /**
+      * Method to call grep command locally.
+      */
+     public void localLogQuerier()
+     {
+         
+     }
+     /**
+      * Method to compare the output of the distributed grep and local grep
+      */
+     public void compareLogs()
+     {
+         
+     }
 }
