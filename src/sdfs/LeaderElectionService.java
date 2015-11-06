608ab0a5b2359c7e50996637849ab5af8f1da7db
nehachaube <nehachaube30@gmail.com>
2015-11-05 18:21:08 -0600

    Added logic for LeaderElectionService

--- src/sdfs/LeaderElectionService.java
+++ src/sdfs/LeaderElectionService.java
@@ -1,11 +1,22 @@
 package sdfs;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+import membershipservice.GossipGroupMembershipService;
 import system.DaemonService;
 import system.Identity;
+/**
+ * LeaderElectionService is a class used to select a new leader
+ * when the master node fails.A node which has the oldest timestamp 
+ * in the membership list is selected as the leader.
+ */
 
 public class LeaderElectionService implements DaemonService {
+     private GossipGroupMembershipService ggms;
 
     @Override
     public void startServe() throws IOException {
@@ -20,6 +31,27 @@ public class LeaderElectionService implements DaemonService {
     }
     
     public Identity getLeaderIdentity() {
-        return null;
+        List<Identity> alive=ggms.getMembershipList().getAliveMembersIncludingSelf();
+        Collections.sort(alive,new MyComparator());
+        System.out.println("Sorted timestamp entries in ascending order");
+        for(Identity timestamp:alive)
+        {
+          System.out.println(timestamp);
+        }
+        return alive.get(0); //returning node with the oldest timestamp as the leader
+    }
+    
+    //sorting each node in ascending order on the basis of timestamp
+    public class MyComparator implements Comparator<Identity>{
+     @Override
+     public int compare(Identity i1, Identity i2)
+     {
+         if(i1.timestamp>i2.timestamp)
+         {
+             return 1; 
+         } else{
+                 return -1;
+             }
+     }
     }
 }
