12d932604a2beb56fbcea23319e8fd82c4b1418f
nehachaube <nehachaube30@gmail.com>
2015-12-05 16:55:29 -0600

    Added Parallelism in Topology

--- src/stormapp/CountTopicTopology.java
+++ src/stormapp/CountTopicTopology.java
@@ -2,19 +2,32 @@ package stormapp;
 
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
 
 public class CountTopicTopology {
     static final String TOPOLOGY_NAME = "storm-twitter-topic-count";
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception, InvalidTopologyException {
         Config config = new Config();
         config.setMessageTimeoutSecs(120);
-        TopologyBuilder b = new TopologyBuilder();
-        b.setSpout("LineSpout", new LineSpout("TT-annotations.csv",';',false));
-        b.setBolt("CountTopicBolt", new FilterBolt()).shuffleGrouping("LineSpout");
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("LineSpout", new LineSpout("TT-annotations.csv",';',false));
+        builder.setBolt("CountTopicBolt", new FilterBolt()).shuffleGrouping("LineSpout");
         
+        config.setDebug(true);
+
+
+        if (args != null && args.length > 0) {
+          config.setNumWorkers(3);
+
+          StormSubmitter.submitTopology(args[0], config, builder.createTopology());
+        }
+        else {
+          config.setMaxTaskParallelism(3);
         final LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology(TOPOLOGY_NAME, config, b.createTopology());
+        cluster.submitTopology(TOPOLOGY_NAME, config, builder.createTopology());
 
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
@@ -23,5 +36,6 @@ public class CountTopicTopology {
                 cluster.shutdown();
             }
         });
+    }
     }
 }
